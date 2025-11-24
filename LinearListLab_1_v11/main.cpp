b34a9913bd2d4c903f3f192e52082cc079b2b8f7
Mariia Surdiur <mariaserduk95@gmail.com>
2025-11-24 19:05:46 +0300

    Initial Commit

---
+++ LinearListLab_1_v11/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LinearListLab_1_v11
+//
+//  Created by Pc on 24.11.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
